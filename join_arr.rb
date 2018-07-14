f5f34dda55cdeb6da5f60409b14aa7cb3c9eaf5c
Soubhagya3 <soubhagya.nayak3@gmail.com>
2018-07-14 13:29:28 +0530

    changes to naming conventions

--- join_arr.rb
+++ join_arr.rb
@@ -1,8 +1,8 @@
-arr1 = Array[1,2,3,4,5]
-arr2 = Array[6,7,8,9,10]
+input1 = Array[1,2,3,4,5]
+input2 = Array[6,7,8,9,10]
 
-arr2.each do |i|
-    arr1 << i
+input2.each do |element|
+    input1 << element
 end
 
-puts "#{arr1}"
+puts "#{input1}"
--- to100.rb
+++ to100.rb
@@ -1,3 +1,3 @@
-for i in 1..100
-    print "#{i} "
+for element in 1..100
+    print "#{element} "
 end
